16480cb429a1ceaa0da52ae69c849ecf67f402fe
Janvi Singh <janvisingh10f@gmail.com>
2026-07-22 19:52:42 +0530

    Rename file to 'exercise 3:Madlib Game.py'

---
+++ exercise 3:Madlib Game.py
@@ -0,0 +1,11 @@
+adjective1= input("enter an adjective (for castle):")
+adjective2= input("enter an adjective (for building):")
+adjective3= input("enter an adjective (for girl):")
+adjective4= input("enter an adjective (for door):")
+noun1= input("enter an noun (for girl):")
+noun2= input("enter an noun (for treasure):")
+noun3= input("enter an noun (ex family, friends):")
+quantity= input("enter an quantity (for the key):")
+
+
+print(f"The {adjective1} Castle. On a hill stood a {adjective1} old castle. Every night, a strange light shone from its {adjective2} tower. One day, a {adjective3} girl named {noun1} decided to explore it. Inside, she found a {quantity} golden key. As soon as she picked it up, a {adjective4} door opened. Behind the door was a treasure chest filled with {noun2} and a note that said, For the one brave enough to find it. {noun1} smiled and shared the treasure with her {noun3}. From that day on, the {adjective1} castle became a place of wonder instead of fear.")
