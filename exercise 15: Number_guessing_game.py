065e86cea1835dabe8750529e0f763cdbc808823
Janvi Singh <janvisingh10f@gmail.com>
2026-07-30 22:04:40 +0530

    Add number guessing game functionality
    Implement a number guessing game with user input and feedback.

---
+++ exercise 15: Number_guessing_game.py
@@ -0,0 +1,29 @@
+import random
+
+low = 1
+high = 100
+answer = random.randint(low, high)
+guesses = 0
+is_running = True
+
+print("python number guessing game")
+print(f"Select a number between {low} and {high}")
+
+while is_running:
+	guess = input("Enter your guess")
+
+	if guess.isdigit():
+		guess = int(guess)
+		guesses +=1
+		
+		if guess < low or guess > high:
+			print("The Number is out of range, Retry!")
+		elif guess < answer:
+			print("Too low, try again!")
+		elif guess > answer:
+			print("Too high, try again!")
+		else:
+			print (f"You guessed correctly! Number is {guesses}")
+	else:
+		print("Invaild guess!, Retry!")
+
