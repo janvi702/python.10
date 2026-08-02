c25c0e1bbec5e76ac39edbdaac2fcb8d66a0a556
Janvi Singh <janvisingh10f@gmail.com>
2026-08-02 20:50:55 +0530

    Update student info input from USN to PRN

--- exercise 16: Student_info.py
+++ exercise 16: Student_info.py
@@ -1,19 +1,20 @@
+#Enter student's baic information
 name = input("Enter the student's name: ")
-usn = input("Enter the student's USN: ")
+prn = input("Enter the student's PRN: ")
 # Read marks in three subjects
 marks_subject1 = float(input("Enter marks in subject 1: "))
 marks_subject2 = float(input("Enter marks in subject 2: "))
 marks_subject3 = float(input("Enter marks in subject 3: "))
 # Calculate total marks and percentage
 total_marks = marks_subject1 + marks_subject2 + marks_subject3
-percentage = (total_marks / 300) * 100 # Assuming each subject is out of 100
+percentage = (total_marks / 300) * 100 
 # Display student details and results
 print("\nStudent Details:")
 print("-----------------")
 print(f"Name:{name}")
-print(f"USN: {usn}")
+print(f"PRN: {prn}")
 print(f"Marks in Subject 1: {marks_subject1}")
 print(f"Marks in Subject 2: {marks_subject2}")
 print(f"Marks in Subject 3: {marks_subject3}")
 print(f"Total Marks: {total_marks}")
-print(f"Percentage: {percentage:.2f}%")
+print(f"Percentage: {percentage:.2f}%")
