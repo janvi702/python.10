c5944146de588b412680fada524853c2dff8ab34
Janvi Singh <janvisingh10f@gmail.com>
2026-08-08 23:33:19 +0530

    Add basic arithmetic functions in Build_calc_using_function.py
    Implemented basic arithmetic functions for addition, subtraction, multiplication, and division with example calls.

---
+++ exercise 19: Build_calc_using_function.py
@@ -0,0 +1,24 @@
+#Defining a function for addition, subtraction, multiplication and division
+#for addition
+def add(x, y):
+  z = x + y
+  return z
+print(add(4, 8))
+
+#for subtraction
+def subtract(x, y):
+  z = x - y
+  return z
+print(subtract(9, 5))
+
+#for multiplicaion
+def multiply(x, y):
+  z = x * y
+  return z
+print(multiply(12, 4))
+
+#for division
+def div(x, y):
+  z = x / y
+  return z
+print(div(564654, 2))
