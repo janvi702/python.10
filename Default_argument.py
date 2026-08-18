4cc1f55ea7a2edea1370255158585d595ce65ea1
Janvi Singh <janvisingh10f@gmail.com>
2026-08-18 22:12:36 +0530

    Add net_price function with discount and tax calculation
    Define a function to calculate net price with discount and tax.

--- Default_argument.py
+++ Default_argument.py
@@ -1,1 +1,6 @@
-
+#defining a function called as "net_price", which include parameters such as list_price, discount and tax
+def net_price (list_price, discount, tax):
+  #it will return the output by using mathemtical formula which is defined by user
+    return list_price * (1- discount) * (1 + tax)
+#numbers have been defined where: list_price=500, discount=0, tax=0.5
+print(net_price(500, 0, 0.5))
