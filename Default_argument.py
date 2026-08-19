44f808ef945dc66a67b71694cb4d0b26db4a879b
Janvi Singh <janvisingh10f@gmail.com>
2026-08-19 23:15:41 +0530

    Update Default_argument.py

--- Default_argument.py
+++ Default_argument.py
@@ -1,6 +1,17 @@
+###1EXAMPLE
 #defining a function called as "net_price", which include parameters such as list_price, discount and tax
 def net_price (list_price, discount, tax):
   #it will return the output by using mathemtical formula which is defined by user
     return list_price * (1- discount) * (1 + tax)
 #numbers have been defined where: list_price=500, discount=0, tax=0.5
 print(net_price(500, 0, 0.5))
+
+
+
+###2EXAMPLE
+#formate of the above exercise
+def num(x, y=0, z=0):
+ return x * (1-y) * (1+z) 
+print(num(500)) 
+#or
+print(num(500, 0.6, 3)) 
