ecda951a092477c0ae14bdf8dcbb5f7280053f94
Janvi Singh <janvisingh10f@gmail.com>
2026-08-20 22:04:55 +0530

    Add circumference and area calculations for a circle

---
+++ exercise 4: Circumference_and_area_of_circle.py
@@ -0,0 +1,15 @@
+#Circumference of a circle
+import math
+Radius= float(input("Enter the number:"))
+#Mathematical formula
+Coc= 2 * math.pi * Radius
+print(f"The circumference of a circle is: {round(Coc, 3)}")
+
+
+
+#Area of a circle
+import math
+R= float(input("Enter the number:"))
+#Mathematical formula
+Areaofcircle=  math.pi * pow(R, 2) #pow(R, 2) maens square of R
+print(f"The area of a circle is: {round(Areaofcircle, 2)}") #round(Areaofcircle, 3) means the output will be upto 2 digits after decimal EX:.54
