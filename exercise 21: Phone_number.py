f199e56b790dbd1cd49a128f900fb0392d4206e7
Janvi Singh <janvisingh10f@gmail.com>
2026-08-20 22:12:36 +0530

    Add phone number formatting function

---
+++ exercise 21: Phone_number.py
@@ -0,0 +1,6 @@
+#defining a function as "num"
+def num(country, area, first, last):
+    return f"{country}-{area}-{first}-{last}"
+#defining the number to different parameters
+phone= num(country=91, area= 7204, first= 22, last= 00)
+print(phone)
