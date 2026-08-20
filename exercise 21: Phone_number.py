fc7fee89c836768a3be663bfbc8055a540d91cd0
Janvi Singh <janvisingh10f@gmail.com>
2026-08-20 22:31:24 +0530

    Fix phone number formatting in Phone_number.py

--- exercise 21: Phone_number.py
+++ exercise 21: Phone_number.py
@@ -2,5 +2,5 @@
 def num(country, area, first, last):
     return f"{country}-{area}-{first}-{last}"
 #defining the number to different parameters
-phone= num(country=91, area= 7204, first= 22, last= 00)
+phone= num(country="91", area= "7204", first= "22", last= "00")
 print(phone)
