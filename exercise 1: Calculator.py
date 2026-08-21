3729c395e334b715bf437df69f9f37f119e41112
Janvi Singh <janvisingh10f@gmail.com>
2026-08-21 21:53:48 +0530

    Update and rename exercise 1 to exercise 1: Calculator.py

---
+++ exercise 1: Calculator.py
@@ -0,0 +1,21 @@
+#area of rectangle calculator
+length = float(input("enter the length of the rectangle: "))
+width = float(input("enter the width of the rectangle: "))  
+area = length * width
+print(f"the area of the rectangle is: {area}")
+
+#area of square calculator
+side = float(input("enter the side of the square: "))
+area = side * side
+print(f"the area of the square is: {area}")
+
+#perimeter of rectangle calculator
+length = float(input("enter the length of the rectangle: "))
+width = float(input("enter the width of the rectangle: "))  
+perimeter = 2 * (length + width)
+print(f"the perimeter of the rectangle is: {perimeter}")
+
+#perimeter of square calculator
+side = float(input("enter the side of the square: "))
+perimeter = 4 * side
+print(f"the perimeter of the square is: {perimeter}")
