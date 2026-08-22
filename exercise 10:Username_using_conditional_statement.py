9bd1126ef249ca6fde1d189c1b764bfa23b4a7dd
Janvi Singh <janvisingh10f@gmail.com>
2026-08-22 19:07:53 +0530

    Add username validation with conditional statements

---
+++ exercise 10:Username_using_conditional_statement.py
@@ -0,0 +1,11 @@
+#to make a username
+username= input("Enter the username: ")
+#using if, elif and else statement
+if len(username) > 12:
+    print("The username cannot be more than 12 characters")
+elif not username.find(" ")== -1:
+    print("The username cannot contain space")
+elif not username.isalpha():
+    print("The username cannot contain number")
+else:
+    print(f"Welcome {username}")
