b376bf02ab00f9d3c13d490aa5b223dfbb231cc8
Janvi Singh <janvisingh10f@gmail.com>
2026-08-22 19:05:48 +0530

    Fix formatting and clean up Timer.py

---
+++ exercise 9: Timer.py
@@ -0,0 +1,13 @@
+#Timer mini project
+import time
+
+my_time= int(input("Enter the time in seconds: "))
+#using for loop
+for x in range(my_time, 0, -1):
+    hours= int(x / 3600)
+    minutes= int(x / 60) % 60
+    seconds= x % 60
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
+    time.sleep(1)
+#outside the loop
+print("Time's up!")
