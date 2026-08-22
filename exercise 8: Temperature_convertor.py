154ab34c47240c31fabdbdb5f80dccbecd525900
Janvi Singh <janvisingh10f@gmail.com>
2026-08-22 19:04:23 +0530

    Implement temperature converter from Celsius
    Added a temperature converter that converts Celsius to Fahrenheit and Kelvin.

---
+++ exercise 8: Temperature_convertor.py
@@ -0,0 +1,9 @@
+#temperature convertor from celcius to F and K
+#for Celsius
+C = float(input("Enter the temperature (in Celsius): ")) 
+# for Fahrenheit
+F = (( C * 1.8 ) + 32 ) 
+print(f"The temperature (in Fahrenheit) is {F} ") 
+#for kelvin
+K = ( C + 273.15 ) 
+print(f"The temperature (in kelvin) is {K} ") 
