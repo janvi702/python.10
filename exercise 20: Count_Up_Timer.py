a2ceb856f564a5f4ab7c33d7af6bb97eb9c6ec9a
Janvi Singh <janvisingh10f@gmail.com>
2026-08-23 19:09:11 +0530

    Update exercise 20: Count_Up_Timer.py

--- exercise 20: Count_Up_Timer.py
+++ exercise 20: Count_Up_Timer.py
@@ -10,3 +10,18 @@ def count(start, end):
     print("Congratulationssssss!")
 #defining the starting and ending time
 count(1, 10)
+
+#OR
+# For default setting
+import time
+#defining a function to count
+def count(end, start= 0):
+        #using for loop to add (end=1) number one by one
+    for x in range(start, end+1):
+        print(x)
+            #sleep time will be of 1 second
+        time.sleep(1)
+            #At last, the line will print "Congratulationssssss"
+    print("Congratulationssssss!")
+#defining the starting and ending time
+count(35, 10)
