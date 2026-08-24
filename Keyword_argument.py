e80cb4494230a2643ecb9b90ab39b2e2112657e9
Janvi Singh <janvisingh10f@gmail.com>
2026-08-24 23:15:49 +0530

    Add examples for keyword arguments in hello function
    Added examples of using keyword arguments in the hello function.

--- Keyword_argument.py
+++ Keyword_argument.py
@@ -2,3 +2,7 @@
 def hello(greeting, first, last) :
  print(f"{greeting} {first} {last}")
 hello("Hello", "Janvi", " Singh") 
+#Or 
+hello("Hello", first= "Janvi", last= "Singh") 
+#Or
+hello("Hello", last= "Singh", first= " Janvi") 
