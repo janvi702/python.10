65f0bbcb3fd0a85eb27ad365ee3e4c138989c9b2
Janvi Singh <janvisingh10f@gmail.com>
2026-08-31 20:58:22 +0530

    Update Arbitrary_Constant.py

--- Arbitrary_Constant.py
+++ Arbitrary_Constant.py
@@ -4,3 +4,10 @@
 def add(*num):
     print(type(num) ) 
 add(1,2,3)
+
+#To display a name
+Def add(*args) :
+ Total= 0
+ For arg in args:
+  Print(arg, end= " ") 
+Display_name(" Ms.", "Janvi", " Singh") 
