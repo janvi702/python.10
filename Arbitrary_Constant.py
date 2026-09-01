1d17846c1a4279c2d139ecb29867aa274707edd5
Janvi Singh <janvisingh10f@gmail.com>
2026-09-01 20:13:18 +0530

    Enhance Arbitrary_Constant.py with print_address function
    Added a function to print address using keyword arguments.

--- Arbitrary_Constant.py
+++ Arbitrary_Constant.py
@@ -1,6 +1,6 @@
 #Arbitary arguments for multiple arguments
 #*args= allows to pass multiple non-key arguments
-
+#**kwargs= allows you to pass multiple keyword arguments
 def add(*num):
     print(type(num) ) 
 add(1,2,3)
@@ -11,3 +11,11 @@ Def add(*args) :
  For arg in args:
   Print(arg, end= " ") 
 Display_name(" Ms.", "Janvi", " Singh") 
+
+#To display address
+def print_address(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}:{value}")
+print_address(state= "Maharashtra"
+             City= "Pune"
+             Area= "Kalyani nagar")
