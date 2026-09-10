dbf4607f67396b9b2635ebcc70b25b0f4d4c7a32
Janvi Singh <janvisingh10f@gmail.com>
2026-09-10 20:04:57 +0530

    Update Iterable.py

--- Iterable.py
+++ Iterable.py
@@ -1,1 +1,15 @@
-##
+#to print num 100, 200, 300
+int x= (100, 200, 300) 
+print(x) 
+
+#instead of printing normal variables, we use iterables
+li = [100, 200, 300]
+it = iter(li)
+
+# Iterate until StopIteration is raised
+while True:
+    try:
+        print(next(it))
+    except StopIteration:
+        print("End of iteration")
+        break
